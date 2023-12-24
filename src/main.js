ac1be192df686a5bcea36899575d3997cca3856a
bLue <tbdblue@gmail.com>
2023-12-25 00:28:19 +0800

    feat: SSG & CI support

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
 import './assets/css/main.css'
 
-import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg/single-page'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 
-const pinia = createPinia()
-const app = createApp(App)
-
-app.use(pinia)
-app.use(ElementPlus)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
-
-app.mount('#app')
+export const createApp = ViteSSG(App, ({ app }) => {
+  const pinia = createPinia()
+  app.use(pinia)
+  app.use(ElementPlus)
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+})
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -5,12 +6,19 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  experimental: {
+    renderBuiltUrl(filename) {
+      let cdnUrl = process.env.CDN_URL || ''
+      if (!cdnUrl.endsWith('/')) {
+        cdnUrl += '/'
+      }
+      return `${cdnUrl}${filename}`
     }
   }
 })
