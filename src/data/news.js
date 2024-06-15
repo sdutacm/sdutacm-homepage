9e26120d263e3fa88f01063c0f063e0bd8a3762e
张晓硕 <1197219739@qq.com>
2024-06-15 21:36:22 +0800

    up: 新闻

--- src/data/news.js
+++ src/data/news.js
@@ -3,6 +3,9 @@ import img4Url from '../assets/img/sdutacm-homepage-news-4.png'
 import img3Url from '../assets/img/sdutacm-homepage-news-3.png'
 import img2Url from '../assets/img/sdutacm-homepage-news-2.png'
 import img1Url from '../assets/img/sdutacm-homepage-news-1.png'
+import imgUrl16th from '../assets/img/sdutacm-homepage-news-16th.jpg'
+import imgUrl20240317 from '../assets/img/sdutacm-homepage-news-20240317.jpg'
+import imgUrl20240526 from '../assets/img/sdutacm-homepage-news-20240526.jpg'
 
 const newsItems = [
   {
@@ -22,13 +25,13 @@ const newsItems = [
     title: 'ICPC济南站 双铜牌',
     summary: '恭喜「开发9G」&「清纯白毛小萝莉」!',
     date: '2023-12-03',
-    imgSrc: img3Url,
+    imgSrc: img3Url
   },
   {
     title: 'ICPC杭州站 银牌',
     summary: '恭喜「张鱼小丸子」!',
     date: '2023-12-10',
-    imgSrc: img2Url,
+    imgSrc: img2Url
   },
   {
     title: '蔚蓝系列赛',
@@ -37,6 +40,27 @@ const newsItems = [
     imgSrc: img1Url,
     link: 'https://acm.sdut.edu.cn/onlinejudge3/posts/35'
   },
+  {
+    title: '共同携手！SDUTACM 乌桕树种植',
+    summary: '欢迎各位校友有空时回来除草、浇水、施肥。',
+    date: '2024-03-17',
+    imgSrc: imgUrl20240317,
+    link: 'https://mp.weixin.qq.com/s/qVgPj0yMidf4WcSP3MCkFw'
+  },
+  {
+    title: '捧杯！CCPC 山东省省赛亚军',
+    summary: '恭喜「清纯白毛小萝莉」在 2024 济南邀请赛 & 山东省赛斩获佳绩！',
+    data: '2024-05-26',
+    imgSrc: imgUrl20240526,
+    link: 'https://mp.weixin.qq.com/s/ESO3wY2sJvN5iYzphkOz7g'
+  },
+  {
+    title: 'SDUTACM 第十六届校赛圆满结束',
+    summary: '让我们一起回望比赛期间的精彩瞬间吧！',
+    date: '2024-05-29',
+    imgSrc: imgUrl16th,
+    link: 'https://mp.weixin.qq.com/s/7T3TDDV9r8q1JJPo-sN7RQ'
+  }
   // todo add more news here
 ]
 
